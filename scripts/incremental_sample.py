af6e7ce30dca339545f40ecefca380e34da8b65c
ternava <t.xheva@gmail.com>
2021-08-10 18:27:24 +0200

    fixed the script for incremental build of curl

--- scripts/incremental_sample.py
+++ scripts/incremental_sample.py
@@ -2,84 +2,56 @@ import git
 import csv
 
 from system_build import system_build_time
+from options import all_options, specialized_files
 from binarysize import calculate_binary_size
 
-# You need to provide the path to the repo of your project
 #repo = git.Repo('/home/xternava/Documents/GitHub/curl-ib/')
 repo = git.Repo('/github/curl/')
 
-# List all branches
-""" for branch in repo.branches:
-    print(branch)
- """
-# Delete all existing branches: git branch | grep -v "master" | xargs git branch -D
-
-# Used options for a minimal configuration in curl
-min_ct_options = []
-    
-lst_options = min_ct_options[:]
-
-# You need to provide the path to the repo where you want to save the results
-#f = open('/home/xternava/Documents/GitHub/Incremental-build/data/buildtime_s1.csv', 'w')
-f = open('/src/data/buildtime_dc1.csv', 'w')
 header = ['Branch', 'Option', 'bt_real', 'bt_user', 'bt_sys', 'BinarySize']
+#f = open('/home/xternava/Documents/GitHub/Incremental-build/data/buildtime_i2.csv', 'w')
+f = open('/src/data/buildtime_di1.csv', 'w')
 writer = csv.writer(f)
 writer.writerow(header)
 
-def clean_build_minimal():
-    # To checkout the main branch
-    repo.git.checkout('master')
+# Add all clean build branches to a list, 
+# so only those branches are incrementaly build
+clean_build_branches = []
 
-    # Create the minimal new branch
-    repo.git.branch('curl-minimal')
-    repo.git.checkout('curl-minimal')
+for branch in repo.branches:
+    if(branch.name[5:] in specialized_files):
+        clean_build_branches.append(branch.name)
 
-    # The project in the minimal branch is compiled and commited
-    build_time_1 = system_build_time(min_ct_options)
-    #bsm = calculate_binary_size("./curl")
-    bsm = calculate_binary_size("/github/curl/src/curl")
+print(clean_build_branches)
 
-    bt = [str(repo.active_branch.name), str(min_ct_options), build_time_1[0], build_time_1[1], build_time_1[2], bsm]
-    writer.writerow(bt)
+def incremental_build():
+    for br in clean_build_branches:
+        print(br)
+        for finx, spec_file in enumerate(specialized_files):
+            if(spec_file != br[5:]):
+                print(br[5:])
+                for idx, spec in enumerate(all_options):
+                    if(idx == finx):
+                        # To checkout the main branch
+                        repo.git.checkout(br)
 
-    print(repo.active_branch.name)
+                        # Create a new branch
+                        repo.git.branch('i'+ str(br) + '-' + str(spec_file))
+                        # To checkout the branch after creating it, to use it
+                        repo.git.checkout('i'+ str(br) + '-' + str(spec_file))
 
-    repo.git.add(all=True)
-    repo.index.commit('clean build curl minimal')
+                        print(repo.active_branch)
+                        build_time = system_build_time(spec)
+                        #bs = calculate_binary_size("./x264")
+                        bs = calculate_binary_size("/github/curl/src/curl")
+                        bt = [str(repo.active_branch), str(spec), build_time[0], build_time[1], build_time[2], bs]
+                        writer.writerow(bt)
 
-def clean_build():
-    # we remove one option at e time from the minimal configuration,
-    # create a branch with its name from the 'master' and then 
-    # compile the project with the remained options in the set and commit it
-    for opt in lst_options:
-        # To checkout the master branch
-        repo.git.checkout('master')
-
-        # Create a new branch
-        repo.git.branch('curl'+ opt[1:])
-
-        # To checkout the branch after creating it, to use it
-        repo.git.checkout('curl'+ opt[1:])
-
-        # Copy the list with minimal "configuration"
-        lst = min_ct_options[:] 
-
-        # Remove the option used to create the branch
-        lst.remove(opt) 
-
-        build_time_2  = system_build_time(lst)
-        #bs = calculate_binary_size("./curl")
-        bs = calculate_binary_size("/github/curl/src/curl")
-
-        bt = [str(repo.active_branch.name), str(opt), build_time_2[0], build_time_2[1], build_time_2[2], bs]
-        writer.writerow(bt)
-
-        repo.git.add(all=True)
-        repo.index.commit('clean build curl minimal without ' + opt[1:])
-
-def main():
-    clean_build_minimal()
-    clean_build()
+                        # stage all changes (i.e., object files) and commit
+                        repo.git.add(all=True, force=True)
+                        repo.index.commit('incremental build of icurl branch with ' + str(spec_file))
+def main(): 
+    incremental_build()
 
 if __name__== "__main__":
    main()
